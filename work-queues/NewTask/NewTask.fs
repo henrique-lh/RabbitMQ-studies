11bbed7de2e5722ea5b013945406517fbcaa8d7c
Luis Henrique <henrique.santos.lhls@gmail.com>
2024-07-02 18:50:33 -0300

    Second step
    Description: learning work queues

---
+++ work-queues/NewTask/NewTask.fs
@@ -0,0 +1,37 @@
+﻿open System
+open System.Text
+open RabbitMQ.Client
+
+let getMessage (args: string[]) =
+    if args.Length > 0 then
+        String.Join(" ", args)
+    else
+        "Hello World!"
+
+[<EntryPoint>]
+let main argv =
+    let factory = ConnectionFactory(HostName = "localhost")
+
+    let connection = factory.CreateConnection()
+    let channel = connection.CreateModel()
+
+    channel.QueueDeclare(queue = "task_queue",
+                        durable = true,
+                        exclusive = false,
+                        autoDelete = false,
+                        arguments = null) |> ignore
+
+    let message: string = getMessage argv
+
+    let body = Encoding.UTF8.GetBytes message
+    let properties = channel.CreateBasicProperties()
+    properties.Persistent <- true
+
+    channel.BasicPublish(exchange = "",
+                        routingKey = "task_queue",
+                        basicProperties = null,
+                        body = body)
+
+    printfn "[x] Sent %s" message
+
+    0
---
+++ work-queues/Worker/Worker.fs
@@ -0,0 +1,40 @@
+﻿open System
+open System.Text
+open RabbitMQ.Client
+open RabbitMQ.Client.Events
+open System.Threading
+
+let factory = ConnectionFactory(HostName = "localhost")
+
+let connection = factory.CreateConnection()
+let channel = connection.CreateModel()
+
+channel.QueueDeclare(queue = "task_queue",
+                    durable = true,
+                    exclusive = false,
+                    autoDelete = false,
+                    arguments = null) |> ignore
+
+channel.BasicQos(prefetchSize = uint32 0, prefetchCount = uint16 1, ``global`` = false)
+
+printfn " [*] Waiting for messages."
+
+let consumer = EventingBasicConsumer(channel)
+consumer.Received.Add(fun ea ->
+    let body = ea.Body.ToArray()
+    let message = Encoding.UTF8.GetString body
+    printfn " [x] Received %s" message
+
+    let dots = message.Split('.').Length - 1
+    Thread.Sleep(dots * 1000)
+    printfn " [x] Done"
+
+    channel.BasicAck(ea.DeliveryTag, false)
+)
+
+channel.BasicConsume(queue = "task_queue",
+                    autoAck = false,
+                    consumer = consumer) |> ignore
+
+printfn " Press [enter] to exit."
+Console.ReadLine() |> ignore
