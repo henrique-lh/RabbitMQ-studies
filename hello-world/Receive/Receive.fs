0c6b8310e4448ae6a08c8cd85b9bfdb57b4beab3
Luis Henrique <henrique.santos.lhls@gmail.com>
2024-07-02 18:50:18 -0300

    First step

---
+++ hello-world/Receive/Receive.fs
@@ -0,0 +1,31 @@
+﻿open System
+open System.Text
+open RabbitMQ.Client
+open RabbitMQ.Client.Events
+
+let factory = ConnectionFactory(HostName = "localhost")
+
+let connection = factory.CreateConnection()
+let channel = connection.CreateModel()
+
+channel.QueueDeclare(queue = "hello",
+                    durable = false,
+                    exclusive = false,
+                    autoDelete = false,
+                    arguments = null) |> ignore
+
+printfn " [*] Waiting for messages."
+
+let consumer = EventingBasicConsumer(channel)
+consumer.Received.Add(fun ea ->
+    let body = ea.Body.ToArray()
+    let message = Encoding.UTF8.GetString body
+    printfn " [x] Received %s" message
+)
+
+channel.BasicConsume(queue = "hello",
+                    autoAck = true,
+                    consumer = consumer) |> ignore
+
+printfn " Press [enter] to exit."
+Console.ReadLine() |> ignore
---
+++ hello-world/Send/Send.fs
@@ -0,0 +1,27 @@
+﻿open System
+open System.Text
+open RabbitMQ.Client
+
+let factory = ConnectionFactory(HostName = "localhost")
+
+let connection = factory.CreateConnection()
+let channel = connection.CreateModel()
+
+channel.QueueDeclare(queue = "hello",
+                    durable = false,
+                    exclusive = false,
+                    autoDelete = false,
+                    arguments = null) |> ignore
+
+let message: string = "Hello World!"
+let body = Encoding.UTF8.GetBytes message
+
+channel.BasicPublish(exchange = "",
+                    routingKey = "hello",
+                    basicProperties = null,
+                    body = body)
+
+printfn "[x] Sent %s" message
+
+printfn " Press [enter] to exit."
+Console.ReadLine() |> ignore
