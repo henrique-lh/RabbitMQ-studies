73ad0e884f62eebf865cd99a29a57f65f6902dfa
Luis Henrique <henrique.santos.lhls@gmail.com>
2024-07-03 11:41:23 -0300

    add rpc studies

---
+++ RPC/RPCClient/RPCClient.fs
@@ -0,0 +1,66 @@
+﻿open System
+open System.Collections.Concurrent
+open System.Text
+open System.Threading
+open System.Threading.Tasks
+open RabbitMQ.Client
+open RabbitMQ.Client.Events
+
+type RpcClient() =
+    let QUEUE_NAME = "rpc_queue"
+
+    let factory = ConnectionFactory(HostName = "localhost")
+    let connection = factory.CreateConnection()
+    let channel = connection.CreateModel()
+    let replyQueueName = channel.QueueDeclare().QueueName
+    let callbackMapper = ConcurrentDictionary<string, TaskCompletionSource<string>>()
+    let consumer = EventingBasicConsumer(channel)
+
+    do
+        consumer.Received.Add(fun ea ->
+            let correlationId = ea.BasicProperties.CorrelationId
+            match callbackMapper.TryRemove(correlationId) with
+            | true, tcs ->
+                let body = ea.Body.ToArray()
+                let response = Encoding.UTF8.GetString body
+                tcs.TrySetResult(response) |> ignore
+            | false, _ -> ()
+        )
+        channel.BasicConsume(consumer = consumer, queue = replyQueueName, autoAck = true) |> ignore
+
+    member this.CallAsync(message: string, cancellationToken: CancellationToken) : Task<string> =
+        let props = channel.CreateBasicProperties()
+        let correlationId = Guid.NewGuid().ToString()
+        props.CorrelationId <- correlationId
+        props.ReplyTo <- replyQueueName
+
+        let messageBytes = Encoding.UTF8.GetBytes message
+        let tcs = TaskCompletionSource<string>()
+        callbackMapper.TryAdd(correlationId, tcs) |> ignore
+
+        channel.BasicPublish(exchange = "", routingKey = QUEUE_NAME, basicProperties = props, body = messageBytes)
+
+        cancellationToken.Register(fun () -> callbackMapper.TryRemove(correlationId) |> ignore) |> ignore
+        tcs.Task
+
+    interface IDisposable with
+        member this.Dispose() =
+            connection.Close()
+
+module Rpc =
+    [<EntryPoint>]
+    let main args =
+        let invokeAsync (n: string) = async {
+            use rpcClient = new RpcClient()
+            printfn " [x] Requesting fib(%s)" n
+            let! response = rpcClient.CallAsync(n, CancellationToken.None) |> Async.AwaitTask
+            printfn " [.] Got '%s'" response
+        }
+
+        printfn "RPC Client"
+        let n = if args.Length > 0 then args.[0] else "30"
+        invokeAsync n |> Async.RunSynchronously
+
+        printfn " Press [enter] to exit."
+        Console.ReadLine() |> ignore
+        0
---
+++ RPC/RPCServer/RPCServer.fs
@@ -0,0 +1,57 @@
+﻿open System
+open System.Text
+open RabbitMQ.Client
+open RabbitMQ.Client.Events
+
+let rec Fib n =
+    match n with
+    | 0 | 1 -> n
+    | _ -> Fib(n - 1) + Fib(n - 2)
+
+let factory = ConnectionFactory(HostName = "localhost")
+
+let connection = factory.CreateConnection()
+let channel = connection.CreateModel()
+
+channel.QueueDeclare(queue = "rpc_queue",
+                    durable = false,
+                    exclusive = false,
+                    autoDelete = false,
+                    arguments = null) |> ignore
+
+channel.BasicQos(prefetchSize = uint32 0, prefetchCount = uint16 1, ``global`` = false)
+let consumer = EventingBasicConsumer(channel)
+channel.BasicConsume(queue = "rpc_queue",
+                    autoAck = false,
+                    consumer = consumer) |> ignore
+
+printfn " [*] Awaiting for RPC requests."
+
+consumer.Received.Add(fun ea ->
+    let body = ea.Body.ToArray()
+    let props = ea.BasicProperties
+    let replyProps = channel.CreateBasicProperties()
+    replyProps.CorrelationId <- props.CorrelationId
+
+    let response =
+        try
+            let message = Encoding.UTF8.GetString body
+            let n = message  |> int32
+            printfn $" [.] Fib({n})"
+            (Fib n).ToString()
+        with
+        | exn ->
+            printfn " [.] %s" exn.Message
+            ""
+
+    let responseBytes = Encoding.UTF8.GetBytes response
+    channel.BasicPublish(exchange = "",
+                        routingKey = props.ReplyTo,
+                        basicProperties = replyProps,
+                        body = responseBytes)
+    channel.BasicAck(ea.DeliveryTag, false)
+
+)
+
+printfn " Press [enter] to exit."
+Console.ReadLine() |> ignore
